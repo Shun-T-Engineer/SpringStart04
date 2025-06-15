111c8909ea2c99072ba4b805f75019008855718c
Shun-T-Engineer <shun.0520.1988@gmail.com>
2025-06-15 11:44:49 +0900

    コントローラーの内容記述

--- src/main/java/com/example/demo/controller/WelcomeSpringController.java
+++ src/main/java/com/example/demo/controller/WelcomeSpringController.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeSpringController {
+	
+	@GetMapping("/welcome")
+	public String welcom() {
+		// HTMLテンプレートにwelcom-spring.htmlを指定
+		return "welcom-spring";
+	}
 
 }
